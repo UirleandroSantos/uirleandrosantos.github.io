61d3aa21a99d2c247eac8fc1cad4b31eaf230996
UirleandroSantos <uirleandro.santos.19@gmail.com>
2023-11-24 10:44:25 -0300

    create script

---
+++ script.js
@@ -0,0 +1,7 @@
+let botao = document.querySelector('#botao');
+let opcoes = document.querySelector('#opcoes')
+
+botao.addEventListener('click', function(){
+    opcoes.style.visibility = 'visible';
+    opcoes.style.height = 'auto';
+})
