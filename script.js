4cef605a5cb18d9edd8183bd68fb9330cb70580d
UirleandroSantos <uirleandro.santos.19@gmail.com>
2023-11-24 11:06:36 -0300

    config script

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let botao = document.querySelector('#botao');
 let opcoes = document.querySelector('#opcoes')
 
-botao.addEventListener('click', function(){
+botao.addEventListener('click', () => {
     opcoes.style.visibility = 'visible';
     opcoes.style.height = 'auto';
+    opcoes.style.listStyle = 'none';
+    opcoes.style.display = 'flex';
+    opcoes.style.gap = '1.5rem';
+    opcoes.style.fontSize = '1.5rem'
 })
