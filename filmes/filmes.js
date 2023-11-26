6e9e80b756561ad523361ed0b752bf6c9c5252f5
UirleandroSantos <uirleandro.santos.19@gmail.com>
2023-11-26 15:29:10 -0300

    add filme

---
+++ filmes/filmes.js
@@ -0,0 +1,14 @@
+const filmes = {
+    homemaranha: `<iframe name=Player "" src="https://sinalpublico.com/player3/serverf4hlb.php?vid=HOMARNHATRVSDARNVRSOBETA" frameborder=0 height=400 scrolling=no width=640 allow="encrypted-media" allowFullScreen> </iframe>`,
+    barbie: `<iframe name=Player "" src="https://sinalpublico.com/player3/serverf4hlb.php?vid=BRBIE2023" frameborder=0 height=400 scrolling=no width=640 allow="encrypted-media" allowFullScreen> </iframe>`
+}
+
+const setInnerHTML = (id, content) => {
+    const element = document.querySelector(id);
+    if (element) {
+      element.innerHTML = content;
+    }
+  };
+
+setInnerHTML('#homemaranha', filmes.homemaranha);
+setInnerHTML('#barbie', filmes.barbie);
