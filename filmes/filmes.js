aff4b7e68aacc81ee2929a794443931283488d9e
UirleandroSantos <uirleandro.santos.19@gmail.com>
2023-12-05 20:18:21 -0300

    update

--- filmes/filmes.js
+++ filmes/filmes.js
@@ -3,6 +3,8 @@ const filmes = {
     barbie: `<iframe name=Player "" src="https://sinalpublico.com/player3/serverf4hlb.php?vid=BRBIE2023" frameborder=0 height=400 scrolling=no width=640 allow="encrypted-media" allowFullScreen> </iframe>`,
     theoldguard: `<iframe name=Player "" src="https://sinalpublico.com/player3/serverf5hlb.php?vid=THOLDGRD" frameborder=0 height=400 scrolling=no width=640 allow="encrypted-media" allowFullScreen> </iframe>`,
     themercenary: `<iframe name=Player "" src="https://sinalpublico.com/player3/serverf4hlb.php?vid=OMRCNRS04" frameborder=0 height=400 scrolling=no width=640 allow="encrypted-media" allowFullScreen> </iframe>`,
+    ipmenem4: `<iframe name=Player "" src="https://sinalpublico.com/player3/serverf5hlb.php?vid=OGRNDMSTR4" frameborder=0 height=400 scrolling=no width=640 allow="encrypted-media" allowFullScreen> </iframe>`,
+
 }
 
 const setInnerHTML = (id, content) => {
@@ -16,3 +18,4 @@ setInnerHTML('#homemaranha', filmes.homemaranha);
 setInnerHTML('#barbie', filmes.barbie);
 setInnerHTML('#theoldguard', filmes.theoldguard);
 setInnerHTML('#themercenary', filmes.themercenary);
+setInnerHTML('#ipmen', filmes.ipmenem4);
