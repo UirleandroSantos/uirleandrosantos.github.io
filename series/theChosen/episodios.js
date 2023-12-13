593cf207f5fa3a50bd2bbc79922b87b3dd7e0b5f
UirleandroSantos <uirleandro.santos.19@gmail.com>
2023-12-12 21:35:38 -0300

    update

--- series/theChosen/episodios.js
+++ series/theChosen/episodios.js
@@ -10,14 +10,14 @@ const episodiostemp01 = {
 }
 
 const episodiostemp02 = {
-    ep01: `<iframe name=Player "" src="https://sinalpublico.com/player3/server11hlb.php?vid=THCHSNOSECLHDT02EP01" frameborder=0 height=400 scrolling=no width=640 allow="encrypted-media" allowFullScreen> </iframe>`,
-    ep02: `<iframe name=Player "" src="https://sinalpublico.com/player3/server11hlb.php?vid=THCHSNOSECLHDT02EP02" frameborder=0 height=400 scrolling=no width=640 allow="encrypted-media" allowFullScreen> </iframe>`,
-    ep03: `<iframe name=Player "" src="https://sinalpublico.com/player3/server11hlb.php?vid=THCHSNOSECLHDT02EP03" frameborder=0 height=400 scrolling=no width=640 allow="encrypted-media" allowFullScreen> </iframe>`,
-    ep04: `<iframe name=Player "" src="https://sinalpublico.com/player3/server11hlb.php?vid=THCHSNOSECLHDT02EP04" frameborder=0 height=400 scrolling=no width=640 allow="encrypted-media" allowFullScreen> </iframe>`,
-    ep05: `<iframe name=Player "" src="https://sinalpublico.com/player3/server11hlb.php?vid=THCHSNOSECLHDT02EP05" frameborder=0 height=400 scrolling=no width=640 allow="encrypted-media" allowFullScreen> </iframe>`,
-    ep06: `<iframe name=Player "" src="https://sinalpublico.com/player3/server11hlb.php?vid=THCHSNOSECLHDT02EP06" frameborder=0 height=400 scrolling=no width=640 allow="encrypted-media" allowFullScreen> </iframe>`,
-    ep07: `<iframe name=Player "" src="https://sinalpublico.com/player3/server11hlb.php?vid=THCHSNOSECLHDT02EP07" frameborder=0 height=400 scrolling=no width=640 allow="encrypted-media" allowFullScreen> </iframe>`,
-    ep08: `<iframe name=Player "" src="https://sinalpublico.com/player3/server11hlb.php?vid=THCHSNOSECLHDT02EP08" frameborder=0 height=400 scrolling=no width=640 allow="encrypted-media" allowFullScreen> </iframe>`
+    ep01: `<iframe name=Player "" src="https://sinalpublico.com/player3/server16hlb.php?vid=THCHSNOSECLHDT02EP01" frameborder=0 height=400 scrolling=no width=640 allow="encrypted-media" allowFullScreen> </iframe>`,
+    ep02: `<iframe name=Player "" src="https://sinalpublico.com/player3/server16hlb.php?vid=THCHSNOSECLHDT02EP02" frameborder=0 height=400 scrolling=no width=640 allow="encrypted-media" allowFullScreen> </iframe>`,
+    ep03: `<iframe name=Player "" src="https://sinalpublico.com/player3/server16hlb.php?vid=THCHSNOSECLHDT02EP03" frameborder=0 height=400 scrolling=no width=640 allow="encrypted-media" allowFullScreen> </iframe>`,
+    ep04: `<iframe name=Player "" src="https://sinalpublico.com/player3/server16hlb.php?vid=THCHSNOSECLHDT02EP04" frameborder=0 height=400 scrolling=no width=640 allow="encrypted-media" allowFullScreen> </iframe>`,
+    ep05: `<iframe name=Player "" src="https://sinalpublico.com/player3/server16hlb.php?vid=THCHSNOSECLHDT02EP05" frameborder=0 height=400 scrolling=no width=640 allow="encrypted-media" allowFullScreen> </iframe>`,
+    ep06: `<iframe name=Player "" src="https://sinalpublico.com/player3/server16hlb.php?vid=THCHSNOSECLHDT02EP06" frameborder=0 height=400 scrolling=no width=640 allow="encrypted-media" allowFullScreen> </iframe>`,
+    ep07: `<iframe name=Player "" src="https://sinalpublico.com/player3/server16hlb.php?vid=THCHSNOSECLHDT02EP07" frameborder=0 height=400 scrolling=no width=640 allow="encrypted-media" allowFullScreen> </iframe>`,
+    ep08: `<iframe name=Player "" src="https://sinalpublico.com/player3/server16hlb.php?vid=THCHSNOSECLHDT02EP08" frameborder=0 height=400 scrolling=no width=640 allow="encrypted-media" allowFullScreen> </iframe>`
 }
 
 const setInnerHTML = (id, content) => {
@@ -36,11 +36,11 @@ setInnerHTML('#temp01ep06', episodiostemp01.ep06);
 setInnerHTML('#temp01ep07', episodiostemp01.ep07);
 setInnerHTML('#temp01ep08', episodiostemp01.ep08);
 
-setInnerHTML('#ep1', episodiostemp02.ep01);
-setInnerHTML('#ep2', episodiostemp02.ep02);
-setInnerHTML('#ep3', episodiostemp02.ep03);
-setInnerHTML('#ep4', episodiostemp02.ep04);
-setInnerHTML('#ep5', episodiostemp02.ep05);
-setInnerHTML('#ep6', episodiostemp02.ep06);
-setInnerHTML('#ep7', episodiostemp02.ep07);
-setInnerHTML('#ep8', episodiostemp02.ep08);
+setInnerHTML('#temp02ep01', episodiostemp02.ep01);
+setInnerHTML('#temp02ep02', episodiostemp02.ep02);
+setInnerHTML('#temp02ep03', episodiostemp02.ep03);
+setInnerHTML('#temp02ep04', episodiostemp02.ep04);
+setInnerHTML('#temp02ep05', episodiostemp02.ep05);
+setInnerHTML('#temp02ep06', episodiostemp02.ep06);
+setInnerHTML('#temp02ep07', episodiostemp02.ep07);
+setInnerHTML('#temp02ep08', episodiostemp02.ep08);
