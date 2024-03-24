6a2f518089a510bf3d528d2c08da77afbe004d4f
uirleandrosantos <uirleandro.santos.19@gmail.com>
2024-03-24 15:03:22 -0300

    add Amor e redenção

--- filmes/filmes.js
+++ filmes/filmes.js
@@ -4,7 +4,7 @@ const filmes = {
     theoldguard: `<iframe name=Player "" src="https://sinalpublico.com/player3/serverf5hlb.php?vid=THOLDGRD" frameborder=0 height=400 scrolling=no width=640 allow="encrypted-media" allowFullScreen> </iframe>`,
     themercenary: `<iframe name=Player "" src="https://sinalpublico.com/player3/serverf4hlb.php?vid=OMRCNRS04" frameborder=0 height=400 scrolling=no width=640 allow="encrypted-media" allowFullScreen> </iframe>`,
     ipmenem4: `<iframe name=Player "" src="https://sinalpublico.com/player3/serverf5hlb.php?vid=OGRNDMSTR4" frameborder=0 height=400 scrolling=no width=640 allow="encrypted-media" allowFullScreen> </iframe>`,
-
+    amorERedencao: `<iframe name=Player "" src="https://sinalpublico.cc/player3/serverf5hlb.php?vid=AMRDRDNCAO" frameborder=0 height=400 scrolling=no width=640 allow="encrypted-media" allowFullScreen> </iframe>`,
 }
 
 const setInnerHTML = (id, content) => {
@@ -19,3 +19,4 @@ setInnerHTML('#barbie', filmes.barbie);
 setInnerHTML('#theoldguard', filmes.theoldguard);
 setInnerHTML('#themercenary', filmes.themercenary);
 setInnerHTML('#ipmen', filmes.ipmenem4);
+setInnerHTML('#amorERedencao', filmes.amorERedencao);
